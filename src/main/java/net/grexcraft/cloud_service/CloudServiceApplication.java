c1f92df4204692387c916a1f5e648d86c448a953
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-03-07 01:20:35 +0100

    remove deprecated redis bean

--- src/main/java/net/grexcraft/cloud_service/CloudServiceApplication.java
+++ src/main/java/net/grexcraft/cloud_service/CloudServiceApplication.java
@@ -7,19 +7,7 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @SpringBootApplication
 public class CloudServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CloudServiceApplication.class, args);
     }
-
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConFactory
-                = new JedisConnectionFactory();
-        jedisConFactory.setHostName(System.getenv("GC_REDIS_HOST"));
-        jedisConFactory.setPort(Integer.parseInt(System.getenv("GC_REDIS_PORT")));
-        return jedisConFactory;
-    }
-
-
 }
