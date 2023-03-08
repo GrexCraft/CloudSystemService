18c3cb082411b09dac02f4dd088e670aec7dd972
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-03-08 22:10:42 +0100

    add image model

---
+++ src/main/java/net/grexcraft/cloud_service/model/Image.java
@@ -0,0 +1,23 @@
+package net.grexcraft.cloud_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "image")
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
--- src/main/java/net/grexcraft/cloud_service/model/RedisBungeeEventData.java
+++ src/main/java/net/grexcraft/cloud_service/model/RedisBungeeEventData.java
@@ -3,17 +3,23 @@ package net.grexcraft.cloud_service.model;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
 
+@RedisHash("RedisBungeeEventData")
 @Data
 public class RedisBungeeEventData implements Serializable {
 
     public enum BungeeEventType {
         REGISTER, REMOVE
     }
+
+    @Id
+    Long id;
 
     private String name;
     private String hostname;
