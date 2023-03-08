8bae1ff7370f5d0ec7e2c6b091bd1a99ede288c3
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-03-08 22:19:08 +0100

    fix redis connection

--- src/main/java/net/grexcraft/cloud_service/config/RedisConfig.java
+++ src/main/java/net/grexcraft/cloud_service/config/RedisConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -24,8 +26,9 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 public class RedisConfig {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(System.getenv("GC_CLOUD_REDIS_HOST"), Integer.parseInt(System.getenv("GC_CLOUD_REDIS_PORT")));
+        //redisStandaloneConfiguration.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
